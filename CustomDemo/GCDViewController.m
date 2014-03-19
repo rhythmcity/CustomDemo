025bd699a38f394e1fddcea838adaace6279919d
liyan <707704996@qq.com>
2014-03-19 13:33:34 +0800

    多线程

--- CustomDemo/GCDViewController.m
+++ CustomDemo/GCDViewController.m
@@ -7,8 +7,6 @@
 //
 
 #import "GCDViewController.h"
-int (^succeed) (int a, int b);
-int (^fail) (int c , int d);
 @interface GCDViewController ()
 
 @end
@@ -72,36 +70,62 @@ int (^fail) (int c , int d);
 }
 
 - (IBAction)btn_click:(id)sender {
-//   
- //  int (^myblocks) (int a ,int b ) = NULL;
-//    myblocks = ^(void) {
-//        NSLog(@"in blocks");
-//    };
-//    NSLog(@"before myblocks");
-//    myblocks();
- //    myblocks3(20, 30);
-//    NSLog(@"sum is %d", sum);
-
-    [self getnum:0 succeed:^(int a, int b) {
-        NSLog(@"%d,%d",a,b);
-    } fail:^(int c, int d) {
-        NSLog(@"%d,%d",c,d);
-    }];
-//    [self getnum:^(int a, int b) {
-//        NSLog(@"%d%d",a,b);
+    dispatch_queue_t mian=dispatch_queue_create("123123", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(mian, ^{
+        NSLog(@"1");
+    });
+    dispatch_barrier_async(mian, ^{
+        NSLog(@"2");
+    });
+    dispatch_async(mian, ^{
+        NSLog(@"3");
+    });
+    dispatch_barrier_async(mian, ^{
+        NSLog(@"4");
+    });
+    dispatch_async(mian, ^{
+        NSLog(@"5");
+    });
+    dispatch_barrier_async(mian, ^{
+        NSLog(@"6");
+    });
+   
+    dispatch_apply(5, mian, ^(size_t index) {
+        NSLog(@"dsfas");
+    });
+    dispatch_async(mian, ^{
+        NSLog(@"7");
+    });
+////   
+// //  int (^myblocks) (int a ,int b ) = NULL;
+////    myblocks = ^(void) {
+////        NSLog(@"in blocks");
+////    };
+////    NSLog(@"before myblocks");
+////    myblocks();
+// //    myblocks3(20, 30);
+////    NSLog(@"sum is %d", sum);
+//
+//    [self getnum:0 succeed:^(int a, int b) {
+//        NSLog(@"%d,%d",a,b);
+//    } fail:^(int c, int d) {
+//        NSLog(@"%d,%d",c,d);
 //    }];
-    NSLog(@"dasdsfasdffasdfa");
-//    dispatch_queue_t grounpBack= dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-//    dispatch_async(grounpBack, ^{
-//        NSURL * URL=[NSURL URLWithString:@"http://avatar.csdn.net/2/C/D/1_totogo2010.jpg"];
-//        NSData * DATA=[[NSData alloc]initWithContentsOfURL:URL];
-//        UIImage * image=[[UIImage alloc]initWithData:DATA];
-//        if (!DATA) {
-//        
-//            self.imageView.image=image;
-//        }
-//       // [self request];
-//    
-//    });
+////    [self getnum:^(int a, int b) {
+////        NSLog(@"%d%d",a,b);
+////    }];
+//    NSLog(@"dasdsfasdffasdfa");
+////    dispatch_queue_t grounpBack= dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+////    dispatch_async(grounpBack, ^{
+////        NSURL * URL=[NSURL URLWithString:@"http://avatar.csdn.net/2/C/D/1_totogo2010.jpg"];
+////        NSData * DATA=[[NSData alloc]initWithContentsOfURL:URL];
+////        UIImage * image=[[UIImage alloc]initWithData:DATA];
+////        if (!DATA) {
+////        
+////            self.imageView.image=image;
+////        }
+////       // [self request];
+////    
+////    });
 }
 @end
